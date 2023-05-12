36ea31ef0591412d87974d9e91569117744624a2
user <user@183.101.234.74>
2023-05-12 21:52:12 +0900

    412

--- src/main/java/commision/Controller/CNoticeController.java
+++ src/main/java/commision/Controller/CNoticeController.java
@@ -1,7 +1,10 @@
 package commision.Controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
